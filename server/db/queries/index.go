4652a96670ae7a3729df0f05e1bdc7902e72632a
David Ashe <davidmashe@gmail.com>
2025-07-20 16:48:48 -0400

    improved expenditure display query to not show IGNORED

--- server/db/queries/index.go
+++ server/db/queries/index.go
@@ -8,7 +8,9 @@ func SelectExpendituresWithCategoryNameByUserAndMonth() string {
 		ON e.category_id = bc.id
 		where e.user_id = $1
 		and EXTRACT(MONTH FROM e.date_occurred) - 1 = $2
+		and e.category_id != 41
 		and e.value > 0
+		and e.category_id IS NOT NULL
 	UNION
 	select *, 'uncategorized' as category_name from expenditure e
 		where e.user_id = $1
